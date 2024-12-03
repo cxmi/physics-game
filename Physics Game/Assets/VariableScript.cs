c61a5511488a32debbe12a81e7c128510acf8a91
cxmi <christinexmi@gmail.com>
2024-12-02 23:30:17 -0500

    more quest stuff working

---
+++ Physics Game/Assets/VariableScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariableScript : MonoBehaviour
+{
+    public bool canHitRecord = false;
+    public bool playedMusic = false;
+    public bool canTurnOffLight = false;
+    public bool turnedOffLight = false;
+    public bool finishedMeetingLuke = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
